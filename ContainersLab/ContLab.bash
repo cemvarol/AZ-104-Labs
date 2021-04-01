0caa2aef7a53185177bb9cbeb6193b311ea389a6
cemvarol <cem.varol@outlook.com>
2021-04-01 11:23:25 +0100

    Update and rename RunMe.bash to ContLab.bash

---
+++ ContainersLab/ContLab.bash
@@ -0,0 +1,4 @@
+curl -O https://raw.githubusercontent.com/cemvarol/AZ-104-Labs/main/ContainersLab/containers.bash
+ls -la containers.bash
+chmod +x containers.bash
+./containers.bash
--- ContainersLab/RunMe.bash
+++
@@ -1,4 +0,0 @@
-curl -O https://raw.githubusercontent.com/cemvarol/AZ-104-Labs/main/NWLab/NwLab-Resources.bash
-ls -la NwLab-Resources.bash
-chmod +x NwLab-Resources.bash
-./NwLab-Resources.bash
